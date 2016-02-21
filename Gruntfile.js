25e0365c5b9ec3336b10bfc9e841dd8ed75765f9
Duve, Michael <mduve@designmail.net>
2016-02-21 17:33:22 +0100

    added some reporting on css and js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+    "use strict";
 
     grunt.initConfig({
         banner: '/*! <%= pkg.name %> - v.<%= pkg.version %>, <%= grunt.template.today("yyyymmdd-HHMMss") %>  */\n',
@@ -9,17 +10,61 @@ module.exports = function(grunt) {
                     "param-list-format": "list",
                     "property-list-format": "list"
                 },
-                src: 'plugin/src/**/*.js',
+                src: 'plugin/src/js/**/*.js',
                 dest: 'docs/plugin.md'
+            }
+        },
+        plato: {
+            options: {
+                jshint: grunt.file.readJSON('.jshintrc')
+            },
+            plugin: {
+                files: {
+                    'reports/plugin/js': ['plugin/src/js/**/*.js']
+                }
+            }
+        },
+        cssstats: {
+            options: {
+                jsonOutput: true,
+                htmlOutput: true,
+                addHtmlStyles: true,
+                addGraphs: true
+            },
+            plugin: {
+                files: {
+                    'reports/plugin/css': ['plugin/dist/styles/**.css']
+                }
+            }
+        },
+        sass: {
+            options: {
+                sourcemap: 'none'
+            },
+            dev: {
+                options: {
+                    style: 'expanded',
+                    lineNumbers: true
+                },
+                files: {
+                    'plugin/dist/styles/mappedJS.css': 'plugin/src/styles/**/*.scss'
+                }
             }
         }
     });
 
     // Load npm tasks
-    grunt.loadNpmTasks('grunt-jsdoc-to-markdown')
+    grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
+    grunt.loadNpmTasks('grunt-plato');
+    grunt.loadNpmTasks('grunt-css-statistics');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     // Register grunt tasks
     grunt.registerTask('default', []);
     grunt.registerTask('docs', ["jsdoc2md:plugin"]);
+    grunt.registerTask('report', ["plato:plugin"]);
+    grunt.registerTask('stats', ["cssstats:plugin"]);
+    grunt.registerTask('scss', ["sass:plugin"]);
+
 
 };
