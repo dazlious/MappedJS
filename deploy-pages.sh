27ae3e2e9d9b26dd3e27b77718ab5412dd23e882
Duve, Michael <mduve@designmail.net>
2016-02-23 14:58:14 +0100

    deploy pages script updated

--- deploy-pages.sh
+++ deploy-pages.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-git add ./reports
-git add ./docs
-git add ./README.md
-
-git commit -m 'Updated docs/reports'
-git push origin master
-
-rm -rf ~/mappedJS
-mkdir ~/mappedJS
-cp -R ./reports ~/mappedJS/reports
-cp -R ./docs ~/mappedJS/docs
-cp -R ./README.md ~/mappedJS/README.md
-
 git checkout gh-pages
-
-rm -rf reports
-rm -rf docs
-
-cp -R ~/mappedJS/reports ./
-cp -R ~/mappedJS/docs ./
-cp -R ./mappedJS/README.md ./
-
-rm -rf ~/mappedJS
-
-git add ./reports
-git add ./docs
-git add ./README.md
-
-git commit -m 'Updated gh-pages docs and reports'
-git push origin gh-pages
-
+git checkout master docs/ reports/
+git commit -m "'Merge' docs and reports from 'master' branch"
+git push origin
 git checkout master
