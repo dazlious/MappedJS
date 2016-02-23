6625204db262693d2d161e5f0407c43fd4704006
Duve, Michael <mduve@designmail.net>
2016-02-23 11:03:21 +0100

    report/docs: test

--- reports/plugin/js/report.history.js
+++ reports/plugin/js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 23 Feb 2016 09:19:03 GMT","total":{"sloc":112,"maintainability":148.5959672058662},"average":{"sloc":56,"maintainability":"74.30"}}]
+__history = [{"date":"Tue, 23 Feb 2016 09:19:03 GMT","total":{"sloc":112,"maintainability":148.5959672058662},"average":{"sloc":56,"maintainability":"74.30"}},{"date":"Tue, 23 Feb 2016 10:02:50 GMT","total":{"sloc":112,"maintainability":148.5959672058662},"average":{"sloc":56,"maintainability":"74.30"}}]
