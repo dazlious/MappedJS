abd2ebf0c460823363f75d7ac3ff0f3b86e049d9
Duve, Michael <mduve@designmail.net>
2016-02-23 11:02:18 +0100

    deploy pages script added

---
+++ deploy-pages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -rf ~/mappedJS
+mkdir ~/mappedJS
+cp -R ./reports ~/mappedJS/reports
+cp -R ./docs ~/mappedJS/docs
+cp -R ./README.md ~/mappedJS/README.md
+
+git checkout gh-pages
+
+rm -rf reports
+rm -rf docs
+
+cp -R ~/mappedJS/reports ./
+cp -R ~/mappedJS/docs ./
+cp -R ./mappedJS/README.md ./
+
+rm -rf ~/mappedJS
+
+git add ./reports
+git add ./docs
+git add ./README.md
+
+git commit -m 'Updated gh-pages docs and reports'
+git push origin gh-pages
+
+git checkout master
