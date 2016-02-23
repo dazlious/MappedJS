937d978e1fc4f1e4d38af3d30655bdd0cbb71d12
Duve, Michael <mduve@designmail.net>
2016-02-23 11:15:55 +0100

    Updated gh-pages docs and reports

--- reports/plugin/js/files/Main_js/report.history.js
+++ reports/plugin/js/files/Main_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 23 Feb 2016 09:19:03 GMT","sloc":49,"lloc":35,"functions":7,"deliveredBugs":0.3071988740793431,"maintainability":75.21676248596845,"lintErrors":[],"difficulty":22.25},{"date":"Tue, 23 Feb 2016 10:02:50 GMT","sloc":49,"lloc":35,"functions":7,"deliveredBugs":0.3071988740793431,"maintainability":75.21676248596845,"lintErrors":[],"difficulty":22.25}]
+__history = [{"date":"Tue, 23 Feb 2016 10:15:52 GMT","sloc":49,"lloc":35,"functions":7,"deliveredBugs":0.3071988740793431,"maintainability":75.21676248596845,"lintErrors":[],"difficulty":22.25}]
