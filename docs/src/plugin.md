fa73573c21df09d0ac33f57a0ba94b1cbab99057
Duve, Michael <mduve@designmail.net>
2016-03-07 17:24:22 +0100

    'Merge' docs and reports from 'master' branch

--- docs/src/plugin.md
+++ docs/src/plugin.md
@@ -1,29 +1,6 @@
-## Members
+<a name="get"></a>
+## get() ⇒ <code>Point</code>
+get center-position of rectangle
 
-<dl>
-<dt><a href="#PUBLISHER">PUBLISHER</a></dt>
-<dd><p>Singleton instance of Publisher</p>
-</dd>
-<dt><a href="#instance">instance</a> : <code>Publisher</code></dt>
-<dd><p>singleton instance</p>
-</dd>
-<dt><a href="#PUBLISHER">PUBLISHER</a></dt>
-<dd><p>Singleton instance of Publisher</p>
-</dd>
-</dl>
-
-<a name="PUBLISHER"></a>
-## PUBLISHER
-Singleton instance of Publisher
-
-**Kind**: global variable  
-<a name="instance"></a>
-## instance : <code>Publisher</code>
-singleton instance
-
-**Kind**: global variable  
-<a name="PUBLISHER"></a>
-## PUBLISHER
-Singleton instance of Publisher
-
-**Kind**: global variable  
+**Kind**: global function  
+**Returns**: <code>Point</code> - center point  
---
+++ test/spec/Rectangle.test.js
@@ -0,0 +1,85 @@
+/*global Rectangle,Point*/
+
+describe('Rectangle', function () {
+    "use strict";
+
+    it("is an instanceof Rectangle", function() {
+        expect(new Rectangle.Rectangle() instanceof Rectangle.Rectangle).toEqual(true);
+    });
+
+    it("initializes with zeros", function() {
+        var rect = new Rectangle.Rectangle();
+        expect(rect.x).toEqual(0);
+        expect(rect.y).toEqual(0);
+        expect(rect.width).toEqual(0);
+        expect(rect.height).toEqual(0);
+    });
+
+    it("initializes correctly with parameters", function() {
+        var rect = new Rectangle.Rectangle(50, 60, 70, 80);
+        expect(rect.x).toEqual(50);
+        expect(rect.y).toEqual(60);
+        expect(rect.width).toEqual(70);
+        expect(rect.height).toEqual(80);
+    });
+
+    it("center is set correctly", function() {
+        var rect = new Rectangle.Rectangle(-50, -50, 100, 100);
+        expect(rect.center).toEqual(new Point.Point(0, 0));
+        rect.x = 0;
+        rect.y = 0;
+        expect(rect.center).toEqual(new Point.Point(50, 50));
+    });
+
+    it("top-left-position is set correctly", function() {
+        var rect = new Rectangle.Rectangle(-50, -50, 100, 100);
+        expect(rect.topLeft).toEqual(new Point.Point(-50, -50));
+        rect.x = 0;
+        rect.y = 0;
+        expect(rect.topLeft).toEqual(new Point.Point(0,0));
+    });
+
+    it("top-right-position is set correctly", function() {
+        var rect = new Rectangle.Rectangle(-50, -50, 100, 100);
+        expect(rect.topRight).toEqual(new Point.Point(50, -50));
+        rect.x = 0;
+        rect.y = 0;
+        expect(rect.topRight).toEqual(new Point.Point(100, 0));
+    });
+
+    it("bottom-left-position is set correctly", function() {
+        var rect = new Rectangle.Rectangle(-50, -50, 100, 100);
+        expect(rect.bottomLeft).toEqual(new Point.Point(-50, 50));
+        rect.x = 0;
+        rect.y = 0;
+        expect(rect.bottomLeft).toEqual(new Point.Point(0, 100));
+    });
+
+    it("bottom-right-position is set correctly", function() {
+        var rect = new Rectangle.Rectangle(-50, -50, 100, 100);
+        expect(rect.bottomRight).toEqual(new Point.Point(50, 50));
+        rect.x = 0;
+        rect.y = 0;
+        expect(rect.bottomRight).toEqual(new Point.Point(100, 100));
+    });
+
+    it("toString works as expected", function() {
+        var rect = new Rectangle.Rectangle(-50, -50, 100, 100);
+        expect(rect.toString()).toEqual("((-50,-50),100,100)");
+    });
+
+    it("equals works fine", function() {
+        var rect1 = new Rectangle.Rectangle(0, 0, 100, 100),
+            rect2 = new Rectangle.Rectangle(1, 0, 100, 100),
+            rect3 = new Rectangle.Rectangle(0, 1, 100, 100),
+            rect4 = new Rectangle.Rectangle(0, 0, 101, 100),
+            rect5 = new Rectangle.Rectangle(0, 0, 100, 101),
+            rect6 = new Rectangle.Rectangle(0, 0, 100, 100);
+        expect(rect1.equals(rect2)).toBe(false);
+        expect(rect1.equals(rect3)).toBe(false);
+        expect(rect1.equals(rect4)).toBe(false);
+        expect(rect1.equals(rect5)).toBe(false);
+        expect(rect1.equals(rect6)).toBe(true);
+    });
+
+});
