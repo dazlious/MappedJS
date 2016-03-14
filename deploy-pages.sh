eec973b7e57bf8936addd53e35f56e4bb24fc480
Duve, Michael <mduve@designmail.net>
2016-03-14 15:29:38 +0100

    deployScript changed

--- deploy-pages.sh
+++ deploy-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git checkout gh-pages
-git checkout master docs/ reports/ test/ plugin/dist/ plugin/examples/
+git checkout master docs/ reports/ test/ vendor/ plugin/dist/ plugin/examples/
 git commit -m "'Merge' docs, reports and examples from 'master' branch"
 git push origin
 git checkout master
