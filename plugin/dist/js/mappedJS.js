255803a216c1cca2da487bdadd250e3ab1899e81
Duve, Michael <mduve@designmail.net>
2016-06-02 20:43:02 +0200

    removed stage-0 from plugins in webpack

--- plugin/dist/js/mappedJS.js
+++ plugin/dist/js/mappedJS.js
@@ -5633,6 +5633,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	var queueIndex = -1;
 
 	function cleanUpNextTick() {
+	    if (!draining || !currentQueue) {
+	        return;
+	    }
 	    draining = false;
 	    if (currentQueue.length) {
 	        queue = currentQueue.concat(queue);
