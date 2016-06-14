6a2ce64d8176cc6c0aabb94f579c32cec36103d6
Duve, Michael <mduve@designmail.net>
2016-06-14 09:34:08 +0200

    deployscript gh-pages updatet

--- deploy-pages.sh
+++ deploy-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git checkout gh-pages
-git checkout master docs/ reports/ test/ vendor/ plugin/dist/ plugin/examples/ plugin/hbs/ es5-transpiled/
+git checkout master docs/ reports/ test/ vendor/ plugin/dist/ plugin/examples/ plugin/hbs/ es5-transpiled/ plugin/img/
 git commit -m "'Merge' docs, reports and examples from 'master' branch"
 git push origin
 git checkout master
